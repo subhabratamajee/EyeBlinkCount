99f01d466ab8968d90674d9b0e499f650c2fd588
Subhabrata Majee <JHS1941JHS@GMAIL.COM>
2022-02-02 11:44:14 +0530

    Update BlinlCounter.py

--- BlinlCounter.py
+++ BlinlCounter.py
@@ -5,14 +5,13 @@ from cvzone.PlotModule import LivePlot
 
 cap = cv2.VideoCapture(0)
 detector = FaceMeshDetector(maxFaces=1)
-plotY = LivePlot(400, 300,[250,400],invert=True)
-
+plotY = LivePlot(640, 480, [250, 400], invert=True)
 
 idList = [22, 23, 24, 26, 110, 130, 157, 158, 159, 160, 161, 243]
 ratioList = []
 blinkCounter = 0
 counter = 0
-color = (255,0,255)
+color = (255, 0, 255)
 while True:
     ret, img = cap.read()
     img, faces = detector.findFaceMesh(img, draw=False)
@@ -30,37 +29,27 @@ while True:
             lengthVer, _ = detector.findDistance(leftEyeLeft, leftEyeRight)
             cv2.line(img, leftUp, leftDown, (0, 255, 0), 2)
             cv2.line(img, leftEyeLeft, leftEyeRight, (0, 255, 0), 2)
-            ratio = (int((lengthVer/lengthHor)*100))
+            ratio = (int((lengthVer / lengthHor) * 100))
             ratioList.append(ratio)
-            if len(ratioList) >50:
+            if len(ratioList) > 50:
                 ratioList.pop(0)
-            ratioAvg = sum(ratioList)/len(ratioList)
+            ratioAvg = sum(ratioList) / len(ratioList)
 
             if ratioAvg > 350 and counter == 0:
                 blinkCounter += 1
                 counter = 1
-                color = (0,255,0)
+                color = (0, 255, 0)
             if counter != 0:
                 counter += 1
-                if counter > 20:
+                if counter > 30:
                     counter = 0
-                    color = (255,0,255)
+                    color = (255, 0, 255)
 
-
-
-            cvzone.putTextRect(img,f'Blink Count : {blinkCounter}',(50,100),colorR=color)
-            imgPlot = plotY.update(ratioAvg,color)
-            # img = cv2.resize(img, (400, 300))
-            # imgStack = cvzone.stackImages([img,imgPlot],2,1)
+            cvzone.putTextRect(img, f'Blink Count : {blinkCounter}', (50, 100), colorR=color)
+            imgPlot = plotY.update(ratioAvg, color)
+            imgStack = cvzone.stackImages([img,imgPlot],2,1)
     else:
-        # img = cv2.resize(img, (320, 240))
         imgStack = cvzone.stackImages([img, img], 2, 1)
 
-
-
-
-    # cv2.imshow("image", imgStack)
-    cv2.imshow("image", img)
-    cv2.imshow("image", imgPlot)
+    cv2.imshow("image", imgStack)
     cv2.waitKey(25)
-
